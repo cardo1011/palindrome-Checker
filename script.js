f1a1d5bbf3266af5ab599d7dc7604220c9eb6c7b
cardo1011 <117056167+cardo1011@users.noreply.github.com>
2024-01-13 11:51:34 -0800

    JS logic added but need to add html element

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-"use strict";
+// "use strict";
+
+const element = document.getElementById("check-btn");
+element.addEventListener("click", function () {
+  document.getElementById;
+});
+
+// NEED TO FIGURE OUT HOW TO HAVE THE TEXT OF THE RETURN STATEMENT TO APPEAR AFTER CHECKING THE STR PROVIDED
+
+// coding logic:
+
+// function(str) {
+// const reversedStr = str.split().reverse().join()
+// if (reversedStr == str) {
+//     return `${reversedStr} is a Palindromed!`
+// } else {
+//     return `${reversedStr} is not a Palindrome`
+// }
+// }
