1e80acd4a692f1eb75d50488295c5d6e23634803
cardo1011 <117056167+cardo1011@users.noreply.github.com>
2024-05-17 14:19:42 -0700

    finished product

--- script.js
+++ script.js
@@ -1,19 +1,33 @@
 // "use strict";
 
-const element = document.getElementById("check-btn");
-element.addEventListener("click", function () {
-  document.getElementById;
+const checkButton = document.getElementById("check-btn");
+const resultBox = document.getElementById("result-container");
+
+function palindromeChecker() {
+  //getting input from text field
+  let input = document.getElementById("input-text").value.trim();
+
+  if (!input) {
+    alert("Please input a value");
+    return;
+  }
+
+  //converting input to a reversed string
+  const reversedStr = input.split("").reverse().join("");
+
+  //checking and returning if string is a palindrome
+  if (reversedStr === input) {
+    resultBox.style.backgroundColor = "blue";
+    resultBox.style.fontSize = "3em";
+    return `${input} is a Palindrome!`;
+  } else {
+    resultBox.style.backgroundColor = "red";
+    resultBox.style.fontSize = "3em";
+    return `${input} is NOT a Palindrome`;
+  }
+}
+
+//event listener calls palindromeChecker when "check" button is clicked
+checkButton.addEventListener("click", function () {
+  resultBox.textContent = palindromeChecker();
 });
-
-// NEED TO FIGURE OUT HOW TO HAVE THE TEXT OF THE RETURN STATEMENT TO APPEAR AFTER CHECKING THE STR PROVIDED
-
-// coding logic:
-
-// function(str) {
-// const reversedStr = str.split("").reverse().join("")
-// if (reversedStr == str) {
-//     return `${reversedStr} is a Palindromed!`
-// } else {
-//     return `${reversedStr} is not a Palindrome`
-// }
-// }
